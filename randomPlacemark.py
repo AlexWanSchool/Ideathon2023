b833e77246e657a8cc9e418d80b77e01028ee7a3
AlexWanSchool <109563934+AlexWanSchool@users.noreply.github.com>
2023-04-22 11:47:07 -0700

    Update randomPlacemark.py

--- randomPlacemark.py
+++ randomPlacemark.py
@@ -2,8 +2,8 @@
 
 import random
 
-latitude = random.randrange(-90, 90)
-longitude = random.randrange(-180, 180)
+latitude = random.randrange(37.346389, 37.368889)
+longitude = random.randrange(120.414167, 120.4325)
 kml = (
    '<?xml version="1.0" encoding="UTF-8"?>\n'
    '<kml xmlns="http://www.opengis.net/kml/2.2">\n'
